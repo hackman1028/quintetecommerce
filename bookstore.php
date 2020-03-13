47d5aba4673339f6b6868f696f275c750df8b9f1
hackman <hackman1028@gmail.com>
2020-03-13 07:50:34 +0545

    Update bookstore.php

--- bookstore.php
+++ bookstore.php
@@ -26,16 +26,25 @@
               </form>
           </div> -->
 
-<!-- Slide Show -->
-<h1> This is booktore</h1>
-<section>
-  <img class="mySlides" src="img/carousel/1.jpg"
-  style="width:100%">
-  <img class="mySlides" src="img/carousel/2.jpg"
-  style="width:100%">
-  <img class="mySlides" src="img/carousel/3.jpg"
-  style="width:100%">
-</section>
+<div class="container-fluid" id="header">
+          <div class="row">
+              <div class="col-md-3 col-lg-3" id="category">
+                  <div style="background:#D67B22;color:#fff;font-weight:800;border:none;padding:15px;"> The Book Shop </div>
+                  <ul>
+                      <li> <a href="Product.php?value=entrance%20exam"> Entrance Exam </a> </li>
+                      <li> <a href="Product.php?value=Literature%20and%20Fiction"> Literature & Fiction </a> </li>
+                      <li> <a href="Product.php?value=Academic%20and%20Professional"> Academic & Professional </a> </li>
+                      <li> <a href="Product.php?value=Biographies%20and%20Auto%20Biographies"> Biographies & Auto Biographies </a> </li>
+                      <li> <a href="Product.php?value=Children%20and%20Teens"> Children & Teens </a> </li>
+                      <li> <a href="Product.php?value=Regional%20Books"> Regional Books </a> </li>
+                      <li> <a href="Product.php?value=Business%20and%20Management"> Business & Management </a> </li>
+                      <li> <a href="Product.php?value=Health%20and%20Cooking"> Health and Cooking </a> </li>
+
+                  </ul>
+              </div>
+            </div>
+          </div>
+
 
 <!-- Book section -->
 <div class="header">
